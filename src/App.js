0aed8159f4e018a3a9090045f6a95a95e19d876b
huynhdthanhtuan <tuan40655@gmail.com>
2021-10-09 23:10:19 +0700

    Upload source code for the first time

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+// *Ex1: counter
+// function App () {
+
+//   const [counter, setCounter] = useState(1);
+
+//   var handleClickButton = () => setCounter(counter => counter + 1);
+
+//   return (
+//     <div className="main">
+//       <span>Number: {counter}</span>
+//       <button onClick={handleClickButton}>Increase</button>
+//     </div>
+//   )
+// }
+
+
+// *Ex2: bill total
+// function App () {
+
+//   const prices = [10, 20, 30, 40];
+
+//   const [total, setTotal] = useState((
+//     prices.reduce(
+//       (total, current) => total + current
+//     )
+//   ));
+
+//   function handleClickButton() {
+//     setTotal(total => total + 100);
+//   }
+
+//   return (
+//     <div className="main">
+//       <span>Total: {total}</span>
+//       <button onClick={handleClickButton}>Increase</button>
+//     </div>
+//   )
+// }
+
+
+// *Ex3: update info
+function App () {
+
+  const [info, setInfo] = useState({
+    name: 'Tuan',
+    age: 21,
+    school: 'DTU'
+  });
+
+  var handleClickButton = function () {
+    setInfo({
+      ...info,
+      school: 'Duy Tan',
+      hobby: 'football'
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div>Info: {JSON.stringify(info)}</div>
+      <button onClick={handleClickButton}>Update</button>
     </div>
-  );
+  )
 }
+
 
 export default App;
