0e48bb633c9a34981806fc4eda4bcee45f95d145
huynhdthanhtuan <tuan40655@gmail.com>
2021-12-05 01:08:20 +0700

    8 & 9. Train & Training function: DONE

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
+import "./App.css";
 import { useEffect, useRef } from "react";
+import { Howl } from "howler";
+import * as tf from "@tensorflow/tfjs";
+import "@tensorflow/tfjs-backend-cpu";
 import * as mobilenet from "@tensorflow-models/mobilenet";
 import * as knnClassifier from "@tensorflow-models/knn-classifier";
-import { Howl } from "howler";
-import "./App.css";
 
-var sound = new Howl({
+const DONT_TOUCH_LABEL = "DONT_TOUCH";
+const TOUCHED_LABEL = "TOUCHED";
+const TRAINING_TIMES = 50;
+
+const sound = new Howl({
   src: ["assests/audio/back.mp3"],
 });
 
 function App() {
   const videoRef = useRef();
+  const mobilenetModel = useRef();
+  const classifierModel = useRef();
 
   const init = async () => {
+    console.log("Init...");
+
     await turnOnCamera();
+
+    console.log("Setup camera done...");
+
+    mobilenetModel.current = await mobilenet.load();
+    classifierModel.current = knnClassifier.create();
+
+    console.log("Setup done, click Train 1 and Train 2...");
   };
 
   const turnOnCamera = () => {
@@ -40,41 +57,38 @@ function App() {
     });
   };
 
-  const stop = async () => {
-    await turnOffCamera();
+  const train = async (label) => {
+    console.log("--> Train " + label);
+
+    for (let i = 0; i < TRAINING_TIMES; i++) {
+      console.log(
+        `Progress ` + parseInt(((i + 1) / TRAINING_TIMES) * 100) + `%`
+      );
+      await training(label);
+    }
   };
 
-  const turnOffCamera = () => {
-    return new Promise((resolve, reject) => {
-      navigator.getUserMedia =
-        navigator.getUserMedia ||
-        navigator.webkitGetUserMedia ||
-        navigator.mzGetUserMedia ||
-        navigator.msGetUserMedia;
+  const training = (label) => {
+    return new Promise(async (resolve) => {
+      // train ảnh
+      const embedding = mobilenetModel.current.infer(videoRef.current, true);
+      // máy học
+      classifierModel.current.addExample(embedding, label);
 
-      if (navigator.getUserMedia) {
-        navigator.getUserMedia(
-          { video: "false" },
-          (stream) => {
-            videoRef.current.srcObject = null;
-          },
-          (error) => {
-            reject(error);
-          }
-        );
-      } else {
-        reject();
-      }
+      await sleep(100);
+      resolve();
     });
   };
 
-  const playSound = () => {
-    sound.play();
+  const sleep = (milisec) => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, milisec);
+    });
   };
 
-  // useEffect(() => {
-  //   init();
-  // }, []);
+  useEffect(() => {
+    init();
+  }, []);
 
   return (
     <div className="main">
@@ -83,14 +97,20 @@ function App() {
       <video ref={videoRef} width="700px" height="480px" autoPlay />
 
       <div className="control">
-        <button className="btn btn-control" onClick={init}>
-          ON
+        <button
+          className="btn btn-control"
+          onClick={() => train(DONT_TOUCH_LABEL)}
+        >
+          TRAIN 1
         </button>
-        <button className="btn btn-control" onClick={stop}>
-          OFF
+        <button
+          className="btn btn-control"
+          onClick={() => train(TOUCHED_LABEL)}
+        >
+          TRAIN 2
         </button>
-        <button className="btn btn-control" onClick={playSound}>
-          WARNING
+        <button className="btn btn-control" onClick={() => {}}>
+          RUN
         </button>
       </div>
     </div>
